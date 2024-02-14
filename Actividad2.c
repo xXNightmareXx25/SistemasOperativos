14844c2746da3b0f616af685341426852246c127
Lord Shadow <cjohnson2001xd@gmail.com>
2024-02-14 12:21:40 -0600

    Update Actividad2.c

--- Actividad2.c
+++ Actividad2.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include "kbhit.h"
 
+
+
 int Cargar(char *nombre_archivo){
     if (nombre_archivo[0] == '\0') { //Esto significa que no se ingresó un nombre de archivo
         printf("Comandos> Modo de uso: cargar nombre_archivo\n");
@@ -61,35 +63,41 @@ int Enter(char *comando) {
     return 1;
 }
 
+int LineaComandos(char *comando, int *j) {
+    if (kbhit()) { // kbhit() devuelve 1 si se ha presionado una tecla y 0 si no
+            comando[(*j)++] = getchar(); // Leer la tecla presionada (caracter)
+            comando[(*j)] = '\0'; // Agregar el caracter nulo al final del comando
+            
+            if (comando[*j-1] == 10) { // 10 es el código ASCII de Enter
+                comando[*j-1] = '\0';
+                int Ejecutar = Enter(comando); // Procesar el comando
+                
+                if (Ejecutar == 0) {
+                    return 0; // Salir del ciclo
+                }
+                *j = 0; // Reiniciar
+                comando[0] = '\0'; // Reiniciar
+            } 
+            
+            if (comando[*j-1] == 127) { // 127 es el código ASCII de backspace
+                comando[*j-2] = '\0'; // Eliminar el último caracter
+                *j -= 2; // Retroceder dos posiciones
+                if (*j < 0) *j = 0; // Evita que j sea negativo
+            }
+
+            printf("Comandos%d> %s\n",(*j),  comando); //Prompt
+            usleep (100000); // Delay de 100 ms
+        }
+    return 0;
+}
+
 int main(void) {
     char comando[100] = ""; // Se inicializa con el caracter nulo para evitar que salgan cosas raras (basura)
     int j = 0; // Contador de caracteres
     printf("Comandos>\n"); //Prompt
 
     while (1) {
-        if (kbhit()) { // kbhit() devuelve 1 si se ha presionado una tecla y 0 si no
-            comando[j++] = getchar(); // Leer la tecla presionada (caracter)
-            comando[j] = '\0'; // Agregar el caracter nulo al final del comando
-            
-            if (comando[j-1] == 10) { // 10 es el código ASCII de Enter
-                comando[j-1] = '\0';
-                int Ejecutar = Enter(comando); // Procesar el comando
-                if (Ejecutar == 0) {
-                    break; // Salir del ciclo
-                }
-                j = 0; // Reiniciar
-                comando[0] = '\0'; // Reiniciar
-            } 
-            
-            if (comando[j-1] == 127) { // 127 es el código ASCII de backspace
-                comando[j-2] = '\0'; // Eliminar el último caracter
-                j -= 2; // Retroceder dos posiciones
-                if (j < 0) j = 0; // Evita que j sea negativo
-            }
-
-            printf("Comandos> %s\n",  comando); //Prompt
-            usleep (100000); // Delay de 100 ms
-        }
+        LineaComandos(comando, &j);
     }
 
     return 0;
